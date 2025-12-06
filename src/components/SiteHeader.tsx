ec4e7eb693995876ce6498f26b34ac9cc2bfb34f
ttymayor <lionhuang914903@gmail.com>
2025-12-07 07:24:26 +0800

    feat: update global styles and implement theme toggle in SiteHeader for improved UI consistency

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -4,6 +4,7 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { usePathname } from "next/navigation";
 import SidebarMenuList from "@/lib/nav";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export function SiteHeader() {
   const pathname = usePathname();
@@ -11,15 +12,21 @@ export function SiteHeader() {
 
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mx-2 data-[orientation=vertical]:h-4"
-        />
-        <h1 className="text-lg font-bold">
-          {navMainItems.find((item) => item.url === pathname)?.title || "Home"}
-        </h1>
+      <div className="flex w-full items-center justify-between gap-1 px-4 lg:gap-2 lg:px-6">
+        <div className="flex items-center gap-2">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mx-2 data-[orientation=vertical]:h-4"
+          />
+          <h1 className="text-lg font-bold">
+            {navMainItems.find((item) => item.url === pathname)?.title ||
+              "Home"}
+          </h1>
+        </div>
+        <div>
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
